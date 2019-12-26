f8f6717bc14b4a7b4d4f71991ef6f1b997e1308a
ltreborn <lookket.111@gmail.com>
2019-12-26 21:12:41 +0700

    Add tables

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import sqlite3 as sql
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,22 +9,94 @@ app = Flask(__name__)
 @app.route('/router/')
 
 def router():
+    con = sql.connect('data/'+'system1'+'.db')
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from snmp")
+    sysrows = cur.fetchall()
 
-    return render_template('router.html')
+    conn = sql.connect('data/'+'in2'+'.db')
+    conn.row_factory = sql.Row
+    curr = conn.cursor()
+    curr.execute("select * from snmpin")
+    udpinrows = curr.fetchall()
+
+    connn = sql.connect('data/'+'out2'+'.db')
+    connn.row_factory = sql.Row
+    currr = connn.cursor()
+    currr.execute("select * from outtable")
+    udpoutrows = currr.fetchall()
+
+    return render_template('router.html', sysrow=sysrows,udpinrow=udpinrows,udpoutrow=udpoutrows)
+
+@app.route('/router2/')
+
+def router2():
+    con = sql.connect('data/'+'system3'+'.db')
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from snmp")
+    sysrows = cur.fetchall(); 
+
+    conn = sql.connect('data/'+'in3'+'.db')
+    conn.row_factory = sql.Row
+    curr = conn.cursor()
+    curr.execute("select * from snmpin")
+    udpinrows = curr.fetchall()
+
+    connn = sql.connect('data/'+'out3'+'.db')
+    connn.row_factory = sql.Row
+    currr = connn.cursor()
+    currr.execute("select * from outtable")
+    udpoutrows = currr.fetchall()
+
+    return render_template('router2.html', sysrow=sysrows,udpinrow=udpinrows,udpoutrow=udpoutrows)
 
 @app.route('/L2switch/')
 
 def L2switch():
+    con = sql.connect('data/'+'system2'+'.db')
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from snmp")
+    sysrows = cur.fetchall(); 
 
-    return render_template('L2switch.html')
+    conn = sql.connect('data/'+'in4'+'.db')
+    conn.row_factory = sql.Row
+    curr = conn.cursor()
+    curr.execute("select * from snmpin")
+    udpinrows = curr.fetchall()
+
+    connn = sql.connect('data/'+'out4'+'.db')
+    connn.row_factory = sql.Row
+    currr = connn.cursor()
+    currr.execute("select * from outtable")
+    udpoutrows = currr.fetchall()
+
+    return render_template('L2switch.html',sysrow=sysrows,udpinrow=udpinrows,udpoutrow=udpoutrows)
 
 @app.route('/L3switch/')
 
 def L3switch():
+    con = sql.connect('data/'+'system4'+'.db')
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from snmp")
+    sysrows = cur.fetchall(); 
 
-    return render_template('L3switch.html')
+    conn = sql.connect('data/'+'in1'+'.db')
+    conn.row_factory = sql.Row
+    curr = conn.cursor()
+    curr.execute("select * from snmpin")
+    udpinrows = curr.fetchall()
 
+    connn = sql.connect('data/'+'out1'+'.db')
+    connn.row_factory = sql.Row
+    currr = connn.cursor()
+    currr.execute("select * from outtable")
+    udpoutrows = currr.fetchall()
 
+    return render_template('L3switch.html',sysrow=sysrows,udpinrow=udpinrows,udpoutrow=udpoutrows)
 
 @app.route('/member/')
 
---
+++ convertfile.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import sqlite3 as sql
+def convertfile():
+
+    for y in range(4):   # number of files
+        f=open("out"+str(y+1)+".txt","r")    
+        fname = f.name.replace('.txt','')
+        conn = sql.connect('data/'+fname+'.db') 
+        conn.execute('CREATE TABLE outtable (oids TEXT, ka TEXT, types TEXT, times TEXT)')
+        conn.close
+        for x in range(150):     # row in a file
+            
+            t=f.readline().split('/')
+            
+            print(fname+" "+t[0])
+            with sql.connect('data/'+fname+".db") as con:        
+                    
+                cur = con.cursor()
+                cur.execute("INSERT INTO outtable (oids,ka,types,times) VALUES (?,?,?,?)",(t[1],t[0],t[2],t[3]) )
+                con.commit()
+        f.close
+convertfile()
